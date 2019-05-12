3928ac47894e1acaf3b4a00b12a241a3c6b5caaa
Paul-louis Delacour <pldelacour@MacBook-Pro-de-Paul-louis.local>
2019-05-12 10:40:04 +0200

    first pl

---
+++ interface.py
@@ -0,0 +1,132 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+import pandas as pd
+
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+markdown_text = '''
+\n
+This is an interface for queries in the database from the 
+website Airbnb 
+	\n In SEARCH you will be able to search a name, a date, 
+a country, a city ... and get all the related information 
+	\n In PREDEFINIED QUERIES you will get the result of 
+predefinied queries we made
+	\n In INSERT/DELETE you will be able to insert a listing, 
+a review, ...
+'''
+
+app.layout = html.Div([
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='HOME', value='home'),
+        dcc.Tab(label='SEARCH', value='search'),
+        dcc.Tab(label='PREDEFINED QUERIES', value='predefined_queries'),
+        dcc.Tab(label='INSERT', value='insert'),
+        dcc.Tab(label='DELETE', value='delete'),
+    ]),
+    html.Div(id='tabs-content')
+])
+
+#####Here are the different layouts for the different modes
+
+##Search
+df = pd.read_csv('CITY.csv')
+PAGE_SIZE = 5
+
+layout_search = dash_table.DataTable(
+    id='table-filtering',
+    columns=[
+        {"name": i, "id": i} for i in sorted(df.columns)
+    ],
+    pagination_settings={
+        'current_page': 0,
+        'page_size': PAGE_SIZE
+    },
+    pagination_mode='be',
+
+    filtering='be',
+    filtering_settings=''
+)
+
+##predefinied queries
+layout_predefinied = html.Div([
+	dcc.Dropdown(
+    options=[
+        {'label': '1', 'value': '1'},
+        {'label': '2', 'value': '2'},
+        {'label': '3', 'value': '3'},
+        {'label': '4', 'value': '4'},
+        {'label': '5', 'value': '5'},
+        {'label': '6', 'value': '6'},
+        {'label': '7', 'value': '7'},
+        {'label': '8', 'value': '8'},
+        {'label': '9', 'value': '9'},
+        {'label': '10', 'value': '10'}
+    ],
+    value='1'
+	), 
+
+])
+
+app.config['suppress_callback_exceptions']=True
+@app.callback(
+    Output('table-filtering', "data"),
+    [Input('table-filtering', "pagination_settings"),
+     Input('table-filtering', "filtering_settings")])
+
+def update_graph(pagination_settings, filtering_settings):
+    print(filtering_settings)
+    filtering_expressions = filtering_settings.split(' && ')
+    dff = df
+    for filter in filtering_expressions:
+        if ' eq ' in filter:
+            col_name = filter.split(' eq ')[0]
+            filter_value = filter.split(' eq ')[1]
+            dff = dff.loc[dff[col_name] == filter_value]
+        if ' > ' in filter:
+            col_name = filter.split(' > ')[0]
+            filter_value = float(filter.split(' > ')[1])
+            dff = dff.loc[dff[col_name] > filter_value]
+        if ' < ' in filter:
+            col_name = filter.split(' < ')[0]
+            filter_value = float(filter.split(' < ')[1])
+            dff = dff.loc[dff[col_name] < filter_value]
+
+    return dff.iloc[
+        pagination_settings['current_page']*pagination_settings['page_size']:
+        (pagination_settings['current_page'] + 1)*pagination_settings['page_size']
+    ].to_dict('records')
+
+
+
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+
+
+def render_content(tab):
+    if tab == 'home':
+        return html.Div([
+            dcc.Markdown(children = markdown_text)
+        ])
+    elif tab == 'search':
+        return layout_search
+    elif tab == 'predefined_queries':
+    	return layout_predefinied
+    elif tab == 'insert':
+    	return html.Div([
+            html.H3('tab content insert')
+        ])
+    elif tab == 'delete':
+    	return html.Div([
+            html.H3('Tab content delete')
+        ])
+    
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
